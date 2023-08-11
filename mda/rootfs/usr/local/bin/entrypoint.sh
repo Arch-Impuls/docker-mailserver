d2258ad365e803507485d32619290e8a534f31df
Arch-Impuls <55735105+Arch-Impuls@users.noreply.github.com>
2023-08-11 07:07:33 +0200

    Update entrypoint.sh

--- mda/rootfs/usr/local/bin/entrypoint.sh
+++ mda/rootfs/usr/local/bin/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+for _var in $(env | cut -f1 -d"="); do
+    case $_var in (*_FILE)
+		# echo "_var : >"$_var"<"
+		
+		_key="${_var%_FILE}"
+		# echo "_key : >"$_key"<"
+		
+		eval "_path=\$$_var"
+		# echo "_path : >"$_path"<"
+		
+		_value="$(cat "$_path")"
+		# echo "_value : >"$_value"<"
+		
+        echo "$_key" set to "$_value"
+		export "$_key"="$_value"
+    ;;esac
+done
+
 if ! [ -r /etc/dovecot/dh.pem.created ]
 then
   echo "Using pre-generated Diffie Hellman parameters until the new one is generated."
